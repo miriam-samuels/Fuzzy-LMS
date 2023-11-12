783a673af2d07d355b1d0b591df0eca3de380040
miriam-samuels <medomiriam@gmail.com>
2023-11-12 18:45:20 +0100

    fix: removed collateral from borrowers

--- internal/model/v1/user/user.go
+++ internal/model/v1/user/user.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"database/sql"
+	"encoding/json"
+	"errors"
 
 	"github.com/miriam-samuels/loan-management-backend/internal/database"
 )
@@ -13,7 +15,7 @@ func (brw *Borrower) FindBorrowerById() sql.Row {
 }
 
 func (brw *Borrower) UpdateBorrower() (*sql.Stmt, error) {
-	query := "UPDATE borrowers SET phone = $1, birth_date = $2, gender = $3, nationality = $4, state_origin = $5, address = $6, passport = $7, signature = $8,  job = $9, job_term = $10, income = $11, deck = $12, has_criminal_record = $13, offences = $14, jail_time = $15, has_collateral = $16, collateral = $17, collateral_docs = $18, kin = $19, guarantor = $20, nin_slip = $21, nin = $22, bvn = $23, bank_name = $24, account = $25, identification = $26, loan_ids = $27, progress = $28 WHERE id = $29"
+	query := "UPDATE borrowers SET phone = $1, birth_date = $2, gender = $3, nationality = $4, state_origin = $5, address = $6, passport = $7, signature = $8,  job = $9, job_term = $10, income = $11, deck = $12, has_criminal_record = $13, offences = $14, jail_time = $15, kin = $16, guarantor = $17, nin_slip = $18, nin = $19, bvn = $20, bank_name = $21, account = $22, identification = $23, loan_ids = $24, progress = $25 WHERE id = $26"
 	stmt, err := database.LoanDb.Prepare(query)
 	return stmt, err
 }
@@ -28,4 +30,12 @@ func (user *User) UpdateUser() (*sql.Stmt, error) {
 	query := "UPDATE users SET phone = $1, birth_date = $2, gender = $3, nationality = $4, state_origin = $5, address = $6, passport = $7, signature = $8, identification = $9, loan_ids = $10 WHERE id = $11"
 	stmt, err := database.LoanDb.Prepare(query)
 	return stmt, err
+}
+
+func (user *Borrower) SetKinJSON(value interface{}) error {
+	bry, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(bry, &user.Kin)
 }
